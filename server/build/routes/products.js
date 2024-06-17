69bf3fbd043956171677bcb56ca16afd20db8394
JawadDev <108939389+Jawad762@users.noreply.github.com>
2024-06-18 02:47:52 +0300

    Delete server/build directory

--- server/build/routes/products.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const products_js_1 = require("../controllers/products.js");
-const routes = async (fastify, _options) => {
-    fastify.get('/products', products_js_1.getProducts);
-};
-exports.default = routes;
